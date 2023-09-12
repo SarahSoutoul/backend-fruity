a22be9a246aaa715f0a0346a05eb2a758388825f
SarahSoutoul <sarahsoutoul.uk@gmail.com>
2023-09-12 16:25:34 +0100

    Added env variables

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 // Importing Express 
+require('dotenv').config()
 const express = require('express');
 const cors = require('cors');
 // Create our server by calling express
 const app = express()
 // Above 1024
-const port = 3000
+const port = process.env.PORT;
+// process.env.PORT
 
 const fruits = require('./fruits.json');
 
